30a31fb1e84ef81c7d0a8bc67f193bcf7c40f324
Zitz59 <dondongudon@gmail.com>
2022-04-03 17:09:53 +0300

    Add component Message

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import styles from './Message.module.css'
 
-function Message() {
+type messageType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+
+function Message(props: messageType) {
     return (
-        <div>
-
+        <div className={styles.messageWrapper}>
+            <div className={styles.messageBlock}>
+                <div>
+                    <img className={styles.avatar} src={props.avatar}/>
+                </div>
+                <div className={styles.message}>
+                    <div className={styles.messageName}>
+                        <h4>{props.name}</h4>
+                    </div>
+                    <div className={styles.messageText}>{props.message}</div>
+                    <div className={styles.messageTime}>
+                        {props.time}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
