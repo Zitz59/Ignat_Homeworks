f6f91060fd095891d6aad5f0309f10acf0921708
Zitz59 <dondongudon@gmail.com>
2022-04-08 17:59:56 +0300

    HW2 Completed without CSS

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from './App.module.css'
 import HW1 from '../../../p2-homeworks/h1/HW1'
+import HW2 from '../../../p2-homeworks/h2/HW2';
 
 function App() {
     return (
         <div className={styles.App}>
             <div>react homeworks:</div>
             <HW1/>
-            {/*<HW2/>*/}
+            <HW2/>
             {/*<HW3/>*/}
             {/*<HW4/>*/}
             {/*<HW5/>*/}
