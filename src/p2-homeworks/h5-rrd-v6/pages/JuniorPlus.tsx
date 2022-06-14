e72bd7d0174ef31626de1a98fba81d61d41af921
Zitz59 <dondongudon@gmail.com>
2022-06-14 12:03:32 +0300

    hw5 add JuniorPlus component

---
+++ src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function JuniorPlus() {
+    return (
+        <div>
+
+            <h6>it's nothing here ... for the time being</h6>
+
+        </div>
+    )
+}
+
+export default JuniorPlus
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
