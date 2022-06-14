b9201312266f4379564767e6f6feb683fde7c867
Zitz59 <dondongudon@gmail.com>
2022-06-14 12:02:46 +0300

    hw5 import styles add  padding for all pages

--- src/p2-homeworks/h5-rrd-v6/HW5.tsx
+++ src/p2-homeworks/h5-rrd-v6/HW5.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { HashRouter } from 'react-router-dom'
 import Header from './header/Header'
 import Pages from './Pages'
+import s from './HW5.module.css'
 
 function HW5() {
     return (
-        <div>
+        <div className={s.pageBlock}>
             {/*в gh-pages лучше работает HashRouter*/}
             <HashRouter>
 
