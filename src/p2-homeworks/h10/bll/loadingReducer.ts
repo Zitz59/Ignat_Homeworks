55631094629f72785ca9b570b5e25542f683229e
Zitz59 <dondongudon@gmail.com>
2022-08-09 19:01:01 +0300

    hw 10 , add reducer logic and types

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,19 @@
-const initState = {
-
+const initialState = {
+    isLoading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state: initialStateType = initialState, action: LoadingActionType) => { // fix any
     switch (action.type) {
-        case '': {
+        case 'SET-LOADING':
+            console.log('action', action)
+            return {...state, isLoading: action.loading}
+        default:
             return state
-        }
-        default: return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+//actions
+export const loadingAC = (loading: boolean) => ({type: 'SET-LOADING', loading} as const) // fix any
+//types
+type LoadingActionType = ReturnType<typeof loadingAC>
+export type initialStateType = typeof initialState
