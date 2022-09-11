a87ce6b188c3438966d1effe04eba06d42b85f6c
Zitz59 <dondongudon@gmail.com>
2022-09-11 11:46:33 +0300

    hw 11 , add setValue to superRange component & connect superDoubleRange from MUI

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import RangeSlider from './common/c8-SuperDoubleRange/RangeSlider';
 
 function HW11() {
     const [value1, setValue1] = useState(0)
@@ -15,6 +16,7 @@ function HW11() {
             <div>
                 <span>{value1}</span>
                 <SuperRange
+                    onChangeRange={setValue1}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
@@ -22,7 +24,12 @@ function HW11() {
             <div>
                 <span>{value1}</span>
                 <SuperDoubleRange
+                    value={[value1,value2]}
+                    // onChangeRange={(value:[setValue1,setValue2])}
                     // сделать так чтоб value1 и value2 изменялось
+                />
+                <RangeSlider
+                    onChangeRange={setValue1}
                 />
                 <span>{value2}</span>
             </div>
