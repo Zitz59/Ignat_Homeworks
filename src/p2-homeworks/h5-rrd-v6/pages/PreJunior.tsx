4efb7be16b7e69b0511352f19270d0f41c968299
Zitz59 <dondongudon@gmail.com>
2022-09-11 16:02:17 +0300

    hw 10 , connect 10 hw

--- src/p2-homeworks/h5-rrd-v6/pages/PreJunior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/PreJunior.tsx
@@ -3,10 +3,11 @@ import HW1 from '../../h1/HW1'
 import HW2 from '../../h2/HW2'
 import HW3 from '../../h3/HW3'
 import HW4 from '../../h4/HW4'
-import HW6 from "../../h6/HW6";
+import HW6 from '../../h6/HW6';
 import HW7 from '../../h7/HW7';
 import HW8 from '../../h8/HW8';
 import HW9 from '../../h9/HW9';
+import HW10 from '../../h10/HW10';
 
 function PreJunior() {
     return (
@@ -19,7 +20,7 @@ function PreJunior() {
             <HW7/>
             <HW8/>
             <HW9/>
-
+            <HW10/>
         </div>
     )
 }
