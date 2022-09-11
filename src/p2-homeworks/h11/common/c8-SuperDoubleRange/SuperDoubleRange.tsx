f4018d409b73a36d54eb5516a1a4e4ad3569ad78
Zitz59 <dondongudon@gmail.com>
2022-09-11 16:05:52 +0300

    hw 11 add slider with handler

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,22 +1,45 @@
 import React from 'react'
+import Slider from '@mui/material/Slider';
+import {Box} from '@mui/material';
+
 
 type SuperDoubleRangePropsType = {
     onChangeRange?: (value: [number, number]) => void
     value?: [number, number]
+    minValue?: number
+    maxValue?: number
+    disabled?: boolean
     // min, max, step, disable, ...
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
-        onChangeRange, value,
+
+        onChangeRange, value, minValue, maxValue, disabled
         // min, max, step, disable, ...
     }
 ) => {
     // сделать самому, можно подключать библиотеки
+    const handleChange = (event: Event, newValue: number | number[]) => {
+        onChangeRange && onChangeRange(newValue as [number, number])
+    }
 
     return (
         <>
-            DoubleRange
+            <Box sx={{width: 290}}>
+                <Slider
+                    getAriaLabel={() => 'range'}
+                    min={minValue ? minValue : 0}
+                    max={maxValue ? maxValue : 100}
+                    value={value}
+                    onChange={handleChange}
+                    disabled={disabled}
+                    valueLabelDisplay="auto"
+                    disableSwap={true}
+                    color={'secondary'}
+                />
+            </Box>
+
         </>
     )
 }
