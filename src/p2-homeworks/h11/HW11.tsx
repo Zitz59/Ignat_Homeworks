575bc3b73a72584f7b89b6529cb99aa1d92dbcc4
Zitz59 <dondongudon@gmail.com>
2022-09-11 16:01:51 +0300

    hw 11 , add styles and components

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,39 +1,43 @@
 import React, {useState} from 'react'
-import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
-import RangeSlider from './common/c8-SuperDoubleRange/RangeSlider';
+import s from './HW11.module.css'
+import {SuperRange} from './common/c7-SuperRange/SuperRange';
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+    const minValue = 0
+    const maxValue = 100
+
+    const [value1, setValue1] = useState<number>(minValue)
+    const [value2, setValue2] = useState<number>(maxValue)
+
+    const onChangeRange = ([value1, value2]: [number, number]) => {
+        setValue1(value1)
+        setValue2(value2)
+    }
 
     return (
         <div>
             <hr/>
             homeworks 11
-
             {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
+            <div className={s.superRangeBlock}>
+                <span className={s.spanStyle}>{value1}</span>
                 <SuperRange
                     onChangeRange={setValue1}
+                    value={value1}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
 
-            <div>
-                <span>{value1}</span>
+            <div className={s.superDoubleSpanBlock}>
+                <span className={s.spanStyle}>{value1}</span>
                 <SuperDoubleRange
-                    value={[value1,value2]}
-                    // onChangeRange={(value:[setValue1,setValue2])}
+                    value={[value1, value2]}
+                    onChangeRange={onChangeRange}
                     // сделать так чтоб value1 и value2 изменялось
                 />
-                <RangeSlider
-                    onChangeRange={setValue1}
-                />
-                <span>{value2}</span>
+                <span className={s.spanStyle2}>{value2}</span>
             </div>
-
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperRange/>*/}
