1f8dcf1bafcc6e78c7dcc3b4f6efde833125c52f
Zitz59 <dondongudon@gmail.com>
2022-09-12 21:27:42 +0300

    feat : connect HW12

--- src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HW11 from '../../h11/HW11';
+import HW12 from '../../h12/HW12';
 
 
 function Junior() {
     return (
         <div>
             <HW11/>
+            <HW12/>
         </div>
     )
 }
