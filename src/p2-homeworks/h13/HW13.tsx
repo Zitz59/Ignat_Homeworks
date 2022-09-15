a22d58d313cd135880f8fed7c9d98f5286c848d0
Zitz59 <dondongudon@gmail.com>
2022-09-15 16:42:12 +0300

    refactor : remove unused imports

--- src/p2-homeworks/h13/HW13.tsx
+++ src/p2-homeworks/h13/HW13.tsx
@@ -1,22 +1,7 @@
-import React, {ChangeEvent} from 'react';
-import s from './HW13.module.css';
-import {useDispatch, useSelector} from 'react-redux';
-import {AppStoreType} from '../h10/bll/store';
-
-import {changeBackgroundAC} from '../h10/bll/backgroundColorReducer';
-import SuperCheckbox from '../h4/common/c3-SuperCheckbox/SuperCheckbox';
-import SuperButton from '../h4/common/c2-SuperButton/SuperButton';
+import React from 'react';
 import {Request} from './common/Request';
 
-
-
 function HW13() {
-
-    // const theme = useSelector<AppStoreType, string>((store) => store.backgroundColor.theme)
-    // const dispatch = useDispatch()
-    // const onchangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-    //     dispatch(changeBackgroundAC(e.currentTarget.value))
-    // }
 
     return (
         <div >
